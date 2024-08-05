a01ea3aebbfcbd35d9c8d21e06cf280b7cf3ad37
reito <cnschwarzer@qq.com>
2024-08-05 12:47:26 +0800

    feat: use better switch design

--- shell/common/options_switches.cc
+++ shell/common/options_switches.cc
@@ -156,7 +156,7 @@ const char kAllowRunningInsecureContent[] = "allowRunningInsecureContent";
 
 const char kOffscreen[] = "offscreen";
 
-const char kOffscreenUseSharedTexture[] = "offscreenUseSharedTexture";
+const char kUseSharedTexture[] = "useSharedTexture";
 
 const char kNodeIntegrationInSubFrames[] = "nodeIntegrationInSubFrames";
 
